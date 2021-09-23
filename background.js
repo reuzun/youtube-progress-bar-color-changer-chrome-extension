043b4af0933b9ec342741478d0ff0715c4f82c5b
Rüştü Efe Uzun <73116832+reuzun@users.noreply.github.com>
2021-09-23 19:01:13 +0300

    not changing except first chapter is fixed.

--- background.js
+++ background.js
@@ -1,5 +1,3 @@
-
-
 let f = () => {
 chrome.tabs.query(
   {currentWindow: true, active : true},
@@ -19,39 +17,5 @@ chrome.tabs.query(
 )
 };
 
-//f();
-
-
 setInterval(f, 1500);
 
-/*
-chrome.browserAction.onClicked.addListener(function(tab) {
-	console.log('clicked!');
-	console.log(tab);
-	
-	chrome.tabs.executeScript(tab.id,{
-    "allFrames" : true,
-    file: 'script.js'
-},function(results){
-    //Handle any results
-	console.log(results);
-});*/
-	
-	/*try{
-	chrome.webNavigation.getAllFrames({tabId:tab.id},function(frames){
-			//Do what you want with the array of frame descriptor Objects
-			console.log(frames);
-	});
-	}catch(err){console.log('error passed!')}*/
-	
-	
-//});
-
-
-/*chrome.tabs.executeScript(4,{
-    frameId: 4,
-    file: 'script.js'
-},function(results){
-    //Handle any results
-	console.log(results);
-});*/
--- script.js
+++ script.js
@@ -6,45 +6,22 @@ var color_unloaded;
 try{
 	chrome.storage.sync.get("ball", function(data) {
 		color_ball = data.ball ? data.ball : '#b86104';
-		document.getElementsByClassName('ytp-scrubber-button')[0].style.backgroundColor = color_ball // for button
+		document.querySelectorAll('.ytp-scrubber-button').forEach( (el) => el.style.backgroundColor = color_ball )
 	});
 
 	chrome.storage.sync.get("untillBall", function(data) {
 		color_untillBall = data.untillBall ?  data.untillBall : '#b86104';
-		document.getElementsByClassName('ytp-play-progress ytp-swatch-background-color')[0].style.backgroundColor = color_untillBall // until button
+		document.querySelectorAll('.ytp-play-progress.ytp-swatch-background-color').forEach( (el) => el.style.backgroundColor = color_untillBall )
 	});
 
 	chrome.storage.sync.get("loaded", function(data) {
 		color_loaded = data.loaded ? data.loaded : '#1d2424';
-		document.getElementsByClassName('ytp-load-progress')[0].style.backgroundColor = color_loaded // loaded place
+		document.querySelectorAll('.ytp-load-progress').forEach( (el) => el.style.backgroundColor = color_loaded )
 	});
 
 	chrome.storage.sync.get("unloaded", function(data) {
 		color_unloaded = data.unloaded ? data.unloaded : '#485454';
-		document.getElementsByClassName('ytp-progress-list')[0].style.backgroundColor = color_unloaded; //unloaded place
+		document.querySelectorAll('.ytp-progress-list').forEach( (el) => el.style.backgroundColor = color_unloaded )
 	});
 }catch(err){/* Pass the error!*/};
 	
-
-
-
-
-
-/*
-	try{
-		document.getElementsByClassName('ytp-scrubber-button')[0].style.backgroundColor = "#b86104" // for button
-		document.getElementsByClassName('ytp-play-progress ytp-swatch-background-color')[0].style.backgroundColor = "#b86104" // until button
-		document.getElementsByClassName('ytp-progress-list')[0].style.backgroundColor = "#485454"; //unloaded place
-		document.getElementsByClassName('ytp-load-progress')[0].style.backgroundColor = "#1d2424" // loaded place
-	}catch(err){
-		// Styling is done!
-		console.log(err)
-	}
-	
-
-*/
-
-			
-
-
-
